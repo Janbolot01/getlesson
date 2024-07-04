60a0da7866e76ee23cab1bbab914056a7f88555c
baktulan <bak2landeveloper@gmail.com>
2024-07-04 12:40:15 +0600

    Added 3 Hospital methods

--- src/dao/daoImpl/HospitalDaoImpl.java
+++ src/dao/daoImpl/HospitalDaoImpl.java
@@ -1,4 +1,43 @@
 package dao.daoImpl;
 
-public class HospitalDaoImpl {
+import dao.HospitalDao;
+import database.Database;
+import models.Hospital;
+
+public class HospitalDaoImpl implements HospitalDao {
+    private Database database;
+
+    @Override
+    public String updateHospital(Long id, Hospital hospital) {
+        for(Hospital hospital1:Database.hospitals){
+            if (hospital1.getId().equals(id)){
+                hospital1.setHospitalName(hospital.getHospitalName());
+                hospital1.setAddress(hospital.getAddress());
+                return "Successfully updated";
+            }
+            else System.out.println("Not found Hospital id");
+        }
+        return null;
+    }
+
+    @Override
+    public String deleteHospitalById(Long id) {
+        for(Hospital hospital:Database.hospitals){
+            if (hospital.getId().equals(id)){
+                Database.hospitals.remove(hospital);
+                return "Successfully deleted";
+            }else System.out.println("Not found Hospital id");
+        }
+        return "Please provide included id";
+    }
+
+    @Override
+    public Hospital getHospitalById(Long hosId) {
+        for (Hospital hospital:Database.hospitals){
+            if (hospital.getId().equals(hosId)){
+                return hospital;
+            }else System.out.println("Not found Hospital");
+        }
+        return null;
+    }
 }
--- src/service/serviceImpl/HospitalServiceImpl.java
+++ src/service/serviceImpl/HospitalServiceImpl.java
@@ -1,4 +1,22 @@
 package service.serviceImpl;
 
-public class HospitalServiceImpl {
+import models.Hospital;
+import service.HospitalService;
+
+public class HospitalServiceImpl implements HospitalService {
+
+    @Override
+    public String updateHospital(Long id, Hospital hospital) {
+        return null;
+    }
+
+    @Override
+    public String deleteHospitalById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Hospital getHospitalById(Long hosId) {
+        return null;
+    }
 }
