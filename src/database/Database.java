84cfc25f202ef397de825e0dd8109e791fd5d809
zanbo <z7024602@gmil.com>
2024-07-04 12:16:36 +0300

    added classes and dao , service

---
+++ src/database/Database.java
@@ -0,0 +1,11 @@
+package database;
+
+import models.Hospital;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+    public static List<Hospital> hospitals = new ArrayList<>();
+}
